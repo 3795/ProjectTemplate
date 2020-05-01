2add777c00bf1fcdfe37c8ccbd4e351340cb8241
3795 <seven.iwi@gmail.com>
2020-05-01 19:52:22 +0800

    修正配置

--- src/main/java/com/gimhealth/CodeGenerator.java
+++ src/main/java/com/gimhealth/CodeGenerator.java
@@ -44,7 +44,7 @@ public class CodeGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc://mysql:47.108.180.43:3306/jyf?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://47.108.180.43:3306/jyf?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
